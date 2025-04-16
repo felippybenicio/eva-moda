8e8b5e0ceae9f5f5d3ad48afcd6a566c9d949fa3
felippybenicio <desenvolvedor.felippybenicio24@gmail.com>
2025-04-15 22:28:32 -0300

    detalhes

--- efeitos.js
+++ efeitos.js
@@ -1,21 +1,21 @@
 AOS.init();
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     const albumComprimido = document.querySelector('#comprimido')
-//     const esposiçãoAlbum = document.querySelector('#vsMobile')
+document.addEventListener('DOMContentLoaded', function () {
+    const albumComprimido = document.querySelector('#comprimido')
+    const esposiçãoAlbum = document.querySelector('#vsMobile')
 
-//     if (window.innerWidth <= 1225 && albumComprimido && esposiçãoAlbum) {
-//         if (window.innerWidth <= 1225 && albumComprimido && esposiçãoAlbum) {
-//             albumComprimido.addEventListener('click', function () {
-//                 if (esposiçãoAlbum.style.display === 'block') {
-//                     esposiçãoAlbum.style.display = 'none'
-//                 } else {
-//                     esposiçãoAlbum.style.display = 'block'
-//                 }
-//             })
-//         }
-//     }
-// })
+    if (window.innerWidth <= 1225 && albumComprimido && esposiçãoAlbum) {
+        if (window.innerWidth <= 1225 && albumComprimido && esposiçãoAlbum) {
+            albumComprimido.addEventListener('click', function () {
+                if (esposiçãoAlbum.style.display === 'block') {
+                    esposiçãoAlbum.style.display = 'none'
+                } else {
+                    esposiçãoAlbum.style.display = 'block'
+                }
+            })
+        }
+    }
+})
 
 
 
